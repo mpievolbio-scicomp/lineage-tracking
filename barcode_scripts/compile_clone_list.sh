1bd933b1719c66afc3666ebef3a173b95cb7b231
Carsten Fortmann-Grote <grotec@.evolbio.mpg.de>
2020-11-11 10:21:16 +0100

    Fix 'compile_clone_list.sh'

--- barcode_scripts/compile_clone_list.sh
+++ barcode_scripts/compile_clone_list.sh
@@ -8,7 +8,8 @@ DESTINATION_DIRECTORY='../data/accepted_clones'
 rm $DESTINATION_DIRECTORY'/'${POPNAME}'_clone_list.tsv'
 
 #compile all lineage for which first flag is "A" or "B"
-for i in {1..10}; do
+# for i in {1..10}; do
+for i in {1..9}; do
 	file=../data/flags/${POPNAME}-BC${i}_flags.tsv
 	echo ${file}
 	awk '((index(substr($2,1,1),"A") != 0) || (index(substr($2,1,1),"B") != 0) ) {print $1}' ${file} >> $DESTINATION_DIRECTORY'/'${POPNAME}'_clone_list.tsv'
